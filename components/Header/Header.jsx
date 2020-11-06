6e9f9e03c328e2c2a2f23a9558a553c39126b5f1
rbenitez <rbenitez@glamit.com.ar>
2020-11-05 21:32:58 -0300

    Creo el componente header y footer

---
+++ components/Header/Header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <div className="contenido-buscador">
+        <div className="menu">
+          <img src="" alt="" />
+        </div>
+        <div className="buscador">
+          <input type="text" />
+        </div>
+      </div>
+      <div className="logo">
+        <img src="" alt="" />
+      </div>
+      <div className="botones">
+        <button></button>
+        <button></button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
