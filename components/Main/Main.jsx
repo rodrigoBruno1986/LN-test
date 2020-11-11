51f94472a01f4a6103658f8e57fbfc5796e34880
rbenitez <rbenitez@glamit.com.ar>
2020-11-10 23:17:47 -0300

    comentarios

--- components/Main/Main.jsx
+++ components/Main/Main.jsx
@@ -4,32 +4,48 @@ import Card from "../Card/Card";
 import fetch from "isomorphic-fetch";
 
 const Main = () => {
-  let [articulos, setArticulos] = useState();
-
+  const [articulos, setArticulos] = useState();
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
+    //Traemos los datos de la api
     fetch("https://api-test-ln.herokuapp.com/articles")
       .then((response) => response.json())
-      .then((data) => setArticulos(data));
+      .then((data) => {
+        setArticulos(data.articles);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setLoading(false);
+        alert("el error que se a obserbado es : " + error);
+      });
   }, []);
 
-  if (!articulos) {
-    console.log("no hay articulos");
-    return null;
+  if (loading) {
+    return <h1>Cargando...</h1>;
   }
+
+  //Chequeamos
+  if (articulos.length === 0) {
+    return <h1>No hay articulos</h1>;
+  }
+
   //Mostramos los que cuentan con el "subtype":"7",
-  const arrayFiltrado = articulos.articles.filter((elemento) => {
-    return +elemento.subtype === 7;
+  const arrayFiltrado = articulos.filter((elemento) => {
+    return elemento.subtype === "7";
   });
 
+  //cortamos el array y que muestre 10 elementos
   const arrayCortado = arrayFiltrado.slice(0, 10);
 
   const tags = [];
 
-  //sacamos los tags de cada articulo
+  //sacamos los tags de cada articulo recorriendo el array cortado
   arrayCortado.forEach((elemento) => {
     tags.push(...elemento.taxonomy.tags);
   });
 
+  console.log(arrayCortado);
+  // agrupamos los tags por titulo
   const tagsAgrupados = tags.reduce((acc, elemento) => {
     const tagExistente = acc[elemento.text];
     if (tagExistente) {
@@ -40,17 +56,18 @@ const Main = () => {
     }
     return {
       ...acc,
-      [elemento.text]: { ...elemento, count: 0 },
+      [elemento.text]: { ...elemento, count: 1 },
     };
   }, {});
+  console.log(tagsAgrupados);
 
+  //ordenamos el array
   const tagsOrdenados = Object.values(tagsAgrupados).sort(
     (a, b) => b.count - a.count
   );
 
+  //cortamos el array
   const tagsCortados = tagsOrdenados.slice(0, 10);
-
-  console.log(arrayCortado);
 
   return (
     <div>
